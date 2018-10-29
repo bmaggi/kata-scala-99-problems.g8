d8c824f649db5f4781cc52e8264d9fb3041683a6
Benoit Maggi <benoit.maggi@gmail.com>
2018-10-29 13:43:20 +0100

    Use g8 insteadof g8Test
    Signed-off-by: Benoit Maggi <benoit.maggi@gmail.com>

--- src/main/g8/src/test/scala/Problem211.scala
+++ src/main/g8/src/test/scala/Problem211.scala
@@ -4,7 +4,9 @@ class Problem211 extends FunSpec with Matchers {
 
   /*
 2.11 (*) Compare the two methods of calculating Euler's totient function.
-Use the solutions of problems 2.09 and 2.10 to compare the algorithms. Take the number of logical inferences as a measure for efficiency. Try to calculate phi(10090) as an example.
+Use the solutions of problems 2.09 and 2.10 to compare the algorithms.
+ Take the number of logical inferences as a measure for efficiency.
+  Try to calculate phi(10090) as an example.
    */
   def compareTotien(number: Int): (Int, Int) = {
     throw new NotImplementedError()
